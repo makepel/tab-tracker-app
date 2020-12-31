3f59040c048801302fcb32fec40c3e2dddba2c8e
Marko Peltomäki <marko.peltomaki@bitonic.fi>
2020-12-31 14:11:06 +0200

    settimg up the registeration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+
+<div>
+<h1>Register</h1>
+<input type="text" name="email" placeholder="email" v-model="email">
+<br>
+<input type="password" name="password" placeholder="password" v-model="password">
+<br>
+<button
+@click="register"
+>Register</button>
+</div>
+
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Register from '@/components/Register'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'helloworld',
       component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
